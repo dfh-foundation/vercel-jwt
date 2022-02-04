068ebb16c82374a44d4726f4db74d5ebc59d94ac
Johnny Walker <jwalker@designsforhealth.com>
2022-02-04 15:27:41 -0500

    build: upgrade other dev dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,6 @@ module.exports = {
   extends: [
     // Use recommended rules from the @typescript-eslint/eslint-plugin
     'plugin:@typescript-eslint/recommended',
-    // Disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'prettier/@typescript-eslint',
     // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors.
     // Make sure this is always the last configuration in the extends array.
     'plugin:prettier/recommended',
